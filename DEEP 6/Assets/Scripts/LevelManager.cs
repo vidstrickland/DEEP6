e85b092a42850a076c5849653f98bd7b48dd3198
vid <vidkid7@gmail.com>
2017-01-03 16:17:00 -0500

    Implemented Level Manager
    Created LevelManager script and prefab that can automatically go to the
    next scene (for Story Mode) or be redirected to any other scene (Menus).
    
    Added several skeleton scenes to test the LevelManager and basic
    application flow.

---
+++ DEEP 6/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start(){
+		if (autoLoadNextLevelAfter <= 0) {
+			Debug.Log ("Level auto load disabled, use a positive number in seconds");
+		} else {
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+
+	public void LoadLevel(string name){
+		Debug.Log ("Level load requested for " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit();
+	}
+
+	public void LoadNextLevel(){
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+
+}
+ 
