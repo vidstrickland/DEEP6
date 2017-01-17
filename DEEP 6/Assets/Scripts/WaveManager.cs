b809acd21458b369870d56b28f1a5994ee580506
vid <vidkid7@gmail.com>
2017-01-17 01:13:52 -0500

    Further Updates to Wave System
    Wave Manager system now exists but isn’t quite functional yet. Need to
    change it from an enemy count system to a “is this the last enemy?”
    system.

---
+++ DEEP 6/Assets/Scripts/WaveManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour {
+
+	public GameObject[] waveArray;
+	public int enemyCount = 0;
+
+	// Use this for initialization
+	void Start () {
+		ResetEnemyCount ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void IncrementEnemyCount(){
+		enemyCount++;
+		print ("Count++!" + enemyCount);
+	}
+
+	public void DecrementEnemyCount(){
+		enemyCount--;
+		print ("Count--!" + enemyCount);
+	}
+	private void ResetEnemyCount(){
+		enemyCount = 0;
+		print ("Enemy Count Reset to " + enemyCount);
+	}
+}
